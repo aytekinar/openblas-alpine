b9647519ccd7efdb912f9f31692e850dc4c99ec3
Arda Aytekin <aytekin@protonmail.com>
2018-11-10 17:40:48 +0100

    Minor improvements to build.sh

--- build.sh
+++ build.sh
@@ -6,16 +6,17 @@ if [[ $1 == "deploy" ]]; then
   exit
 fi
 
-sudo docker run --rm --privileged multiarch/qemu-user-static:register --reset
 cp Dockerfile Dockerfile.${ARCH}
+sed --in-place "s/__IMAGE_ARCH__/${IMAGE_ARCH}/" Dockerfile.${ARCH}
 
-sed -i "s/__IMAGE_ARCH__/${IMAGE_ARCH}/" Dockerfile.${ARCH}
 if [[ ! ${IMAGE_ARCH} == "amd64" ]]; then
+  sudo docker run --rm --privileged multiarch/qemu-user-static:register --reset
   wget --quiet https://github.com/multiarch/qemu-user-static/releases/download/v3.0.0/qemu-${QEMU_ARCH}-static.tar.gz
   tar -xzf qemu-${QEMU_ARCH}-static.tar.gz
-  sed -i "s/__CROSS_COPY__/COPY/" Dockerfile.${ARCH}
-  sed -i "s/__QEMU_ARCH__/${QEMU_ARCH}/" Dockerfile.${ARCH}
+  sed --in-place "s/__CROSS_COPY__/COPY/" Dockerfile.${ARCH}
+  sed --in-place "s/__QEMU_ARCH__/${QEMU_ARCH}/" Dockerfile.${ARCH}
 else
-  sed -i "/__CROSS_COPY__/d" Dockerfile.${ARCH}
+  sed --in-place "/__CROSS_COPY__/d" Dockerfile.${ARCH}
 fi
+
 docker build --tag openblas/alpine:${ARCH} --file Dockerfile.${ARCH} .
